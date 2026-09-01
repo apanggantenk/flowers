2cda6078f2d045028a66d8945d3835bc1010a8ec
project261009 <sampanrambubase26@gmail.com>
2026-09-01 20:50:29 +0700

    Update interval for createFallingItem function
    Changed interval for createFallingItem to reduce load.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-onload = () => {
-  document.body.classList.remove("container");
-};
+// GANTI DARI:
+setInterval(createFallingItem, 280);
+
+// MENJADI:
+setInterval(createFallingItem, 700); // Elemen lebih sedikit, HP jadi lebih ringan & mulus
